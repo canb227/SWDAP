e31805850e0b59fdc807c9ac2b03bc53fd1af69a
Stephen Banghart <stephen.banghart@nist.gov>
2018-01-31 10:42:17 -0500

    Repo Init

---
+++ swdapModel/src/main/java/org/swdapModel/Card.java
@@ -0,0 +1,5 @@
+package org.swdapModel;
+
+public class Card {
+
+}
---
+++ swdapParser/src/main/java/org/swdapParser/ParserApp.java
@@ -0,0 +1,13 @@
+package org.swdapParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class ParserApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
